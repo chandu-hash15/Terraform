f1e4c30aca2f9d382c48c30c9f07ec39ec0c0f77
Chandu <chandutharun42@gmail.com>
2026-02-07 20:08:34 +0530

    added basic infra

---
+++ ec2.tf
@@ -0,0 +1,28 @@
+resource "aws_instance" "terraform" {
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.tf_allow_all.id]
+
+  tags = {
+    Name = "Terraform"
+  }
+}
+
+
+resource "aws_security_group" "tf_allow_all" {
+  name        = "tf_allow_all"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
