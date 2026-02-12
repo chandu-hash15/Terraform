8761f29ecb35163637af49e952a467c252698c16
Chandu <chandutharun42@gmail.com>
2026-02-12 19:45:50 +0530

    add variables folder normally

---
+++ variables/variables.tf
@@ -0,0 +1,62 @@
+variable "ami_instance" {
+
+    type = string
+
+    default = "ami-0220d79f3f480ecf5"
+}
+
+variable "instance_type" {
+
+    default = "t3.micro"
+}
+
+
+variable "tags" {
+
+    default = {
+        
+        Name = "Terraform"
+
+        Terraform = "True"
+
+        Project = "ERP"
+
+        Environment = "Dev"
+    }
+}
+
+
+variable "sg_allow_all" {
+
+    default = "tf_allow_all"
+}
+
+
+variable "ingress_from_port" {
+    
+    default = 0
+}
+
+
+variable "ingress_to_port" {
+    
+    default = 0
+}
+
+
+variable "egress_from_port" {
+    
+    default = 0
+}
+
+
+variable "egress_to_port" {
+    
+    default = 0
+}
+
+
+variable "cidr_block" {
+
+    default = ["0.0.0.0/0"]
+}
